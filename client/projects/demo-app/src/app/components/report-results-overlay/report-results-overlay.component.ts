82f5d713f04ba11e247e01b2310c4b675dc9ad8e
Sweet Acid <antoniu.ilea@gmail.com>
2021-09-28 04:48:34 +0300

    Moved plagiarism-report to root folder

--- client/projects/demo-app/src/app/components/report-results-overlay/report-results-overlay.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-	// tslint:disable-next-line: component-selector
-	selector: 'cr-report-results-overlay',
-	templateUrl: './report-results-overlay.component.html',
-	styleUrls: ['./report-results-overlay.component.scss']
-})
-export class ReportResultsOverlayComponent { }
